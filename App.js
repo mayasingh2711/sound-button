a074e340fc38b62689fbc90fecd7b551fc2e7817
mayasingh2711 <70598078+mayasingh2711@users.noreply.github.com>
2020-10-10 19:02:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import "./App.css";
+import sound1 from './audioClip/1.mp3';
+import {Howl, Howler} from 'howler';
+
+const audioClip = [
+  {sound: sound1, label: 'Reena'}
+]
+
+class App extends Component {
+  SoundPlay = (src) => {
+    const sound = new Howl({
+      src
+    })
+    sound.play();
+  }
+
+  RenderButtonAndSound = ()=>{
+    return audioClip.map((soundObj, index)=>{
+      return(
+        <button key={index} onClick={()=>this.SoundPlay(soundObj.sound)}>
+          {soundObj.label}
+        </button>
+      )
+    })
+  }
+
+  render() {
+    Howler.volume(1.0)
+    return <div className="App">
+      {this.RenderButtonAndSound()}
+    </div>;
+  }
+}
+export default App;
